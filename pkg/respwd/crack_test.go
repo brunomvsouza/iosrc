1b4557f7d9e35aa3ede19e897ec68bae650ede86
Bruno M V Souza <hi@brunosouza.org>
2018-06-16 12:37:39 +0200

    iOS restriction password recoverer

---
+++ pkg/respwd/crack_test.go
@@ -0,0 +1,29 @@
+package respwd_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"bmvs.io/iosrc/pkg/respwd"
+)
+
+func TestCrack(t *testing.T) {
+	var table = []struct {
+		key  string
+		salt string
+		out  string
+		eout error
+	}{
+		{"aI9gkP3NEG+17D8UPyAT1ehRd7g=", "MTIzNDU", "", respwd.ErrInvalidSalt},
+		{"MTIzNDU=", "MTIzNDU=", "", respwd.ErrSorryDidntWork},
+		{"MTIzNDU", "MTIzNDU=", "", respwd.ErrSorryDidntWork},
+		{"hpO5ww==", "aI9gkP3NEG+17D8UPyAT1ehRd7g=", "", respwd.ErrSorryDidntWork},
+		{"aI9gkP3NEG+17D8UPyAT1ehRd7g=", "hpO5ww==", "4212", nil},
+	}
+
+	for _, row := range table {
+		_, err := respwd.Crack(row.key, row.salt)
+		assert.Equal(t, err, row.eout)
+	}
+}
